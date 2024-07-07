dc4cd5ae57b5f15678379aa373b67f1f95d4c994
don caldwell <doncaldwell@dons-Laptop.local>
2024-07-07 07:04:06 -0400

    still working on merge

---
+++ sortflrecfile_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"testing"
+)
+
+//type kvalline struct {
+//	key  []byte
+//	line []byte
+//}
+
+func Test_sortflrecfile(t *testing.T) {
+	var l uint = 32
+	var nrs uint = 1 << 20
+	var nss uint
+	var iomem int64 = 1<<24 + 1<<20
+
+	var klns kvallines
+	var tklns kvallines
+	var err error
+	var nr int
+	dn := "/tmp"
+
+	log.Println("sortflrecfile test")
+
+	rsl := randomstrings(nrs, l)
+
+	fn := path.Join(dn, "sortflrecfiletest")
+	fp, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	for i, _ := range rsl {
+		fmt.Fprintln(fp, rsl[i])
+		nr++
+	}
+
+	log.Printf("sortflrecfile test  %s, %s, %d\n", fn, dn, iomem)
+	klns, fns, err := sortflrecfile(fn, dn, int(l)+1, 0, 0, iomem)
+
+	log.Println("sortflrecfile after klns ", len(klns))
+	log.Println("sortflrecfile after fns ", len(fns))
+
+	for _, f := range fns {
+		mfp, err := os.Open(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tklns, _, err = vlreadn(mfp, 0, 0, 0, iomem)
+		nss += uint(len(tklns))
+	}
+	if nrs != nss {
+		log.Fatal("sortflrecfile test wanted ", nrs, " got ", nss)
+	}
+
+}
---
+++ vlreadn_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"testing"
+)
+
+//type kvalline struct {
+//	key  []byte
+//	line []byte
+//}
+
+func Test_vlreadn(t *testing.T) {
+	var l uint = 32
+	var nrs uint = 1 << 20
+	var iomem int64 = 1 << 30
+	var nr int
+
+	var klns kvallines
+
+	log.Print("vlreadn test")
+
+	dn, err := initmergedir("rdxsort")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dn)
+
+	rsl := randomstrings(nrs, l)
+
+	fn := path.Join(dn, "vlreadn")
+	fp, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	for _, l := range rsl {
+		fmt.Fprintln(fp, l+"\n")
+		nr++
+	}
+
+	_, err = fp.Seek(0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	klns, _, err = vlreadn(fp, 0, 0, 0, iomem)
+	for _, kln := range klns {
+		if len(kln.line) == 0 {
+			log.Fatal("vlreadn len(kln.line) == 0")
+		}
+		if len(kln.key) != len(kln.line) {
+			log.Fatal("vlreadn len(kln.key) != len(kln.line)")
+		}
+		//log.Print(string(kln.line))
+	}
+	if len(klns) != int(nrs) {
+		log.Fatal("vlreadn: expected ", nrs, " got ", len(klns))
+	}
+	log.Print("vlreadn test passed")
+}
